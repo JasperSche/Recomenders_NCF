abd5383eaa94b09e6cdc87eba8e207ccb48c3d9e
Jasper <jasper.scheel2001@gmail.com>
2026-03-26 20:20:02 +0100

    GMF_troch implementation; skeleton code for NeuMF

--- GMF_torch.py
+++ GMF_torch.py
@@ -2,16 +2,56 @@
 Implementation of Generalized Matrix Factorization (GMF) recommender model, using pytorch
 Based on: He Xiangnan et al. Neural Collaborative Filtering. In WWW 2017.
 '''
-import numpy as np
+from os.path import join
+from Dataset import NCFDataset
+from time import time
+from torch.utils.data import DataLoader
+
 import torch
 import torch.nn as nn
+import torch.nn.init as init
+import torch.optim as optim
 import torch.nn.functional as F
 
+import numpy as np
+import argparse
+
+#################### Arguments ####################
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run GMF.")
+    parser.add_argument('--path', nargs='?', default='Data/',
+                        help='Input data path.')
+    parser.add_argument('--dataset', nargs='?', default='ml-1m',
+                        help='Choose a dataset.')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='Number of epochs.')
+    parser.add_argument('--batch_size', type=int, default=256,
+                        help='Batch size.')
+    parser.add_argument('--num_factors', type=int, default=8,
+                        help='Embedding size.')
+    parser.add_argument('--regs', nargs='?', default='[2,2]', #changed to [2,2] since it is the default in pytorch reffering to the l2 (euclidian) norm
+                        help="Regularization for user and item embeddings.")
+    parser.add_argument('--num_neg', type=int, default=4,
+                        help='Number of negative instances to pair with a positive instance.')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='Learning rate.')
+    parser.add_argument('--learner', nargs='?', default='adam',
+                        help='Specify an optimizer: adagrad, adam, rmsprop, sgd')
+    parser.add_argument('--verbose', type=int, default=1,
+                        help='Show performance per X iterations')
+    parser.add_argument('--out', type=int, default=1,
+                        help='Whether to save the trained model.')
+    return parser.parse_args()
+
+def lecun_uniform(tensor):
+    fan_in, _ = init._calculate_fan_in_and_fan_out(tensor)
+    limit = (3 / fan_in) ** 0.5
+    init.uniform_(tensor, -limit, limit)
 
 class GMF(nn.Module):
     def __init__(self, num_users, num_items, latent_dim, regs=[2,2]):
         super().__init__()
-        self.MF_embedding_user = nn.Embedding(
+        self.MF_embedding_user =nn.Embedding(
             num_embeddings=num_users,
             embedding_dim=latent_dim,
             norm_type=regs[0]
@@ -21,19 +61,82 @@ class GMF(nn.Module):
             embedding_dim=latent_dim,
             norm_type=regs[1]
         )
-        self.pred_layer = nn.Linear()
+
+        init.normal_(self.MF_embedding_user.weight)
+        init.normal_(self.MF_embedding_item.weight)
+
+        self.pred_layer = nn.Linear(latent_dim,1)
+        lecun_uniform(self.pred_layer.weight)
 
     def forward(self, u_input, i_input):
         emb_user = self.MF_embedding_user(u_input)
         emb_item = self.MF_embedding_item(i_input)
 
-        user_latent = nn.Flatten(emb_user)
-        item_latent = nn.Flatten(emb_item)
+        user_latent = nn.Flatten()(emb_user)
+        item_latent = nn.Flatten()(emb_item)
 
-        predict_vec = torch.cat(user_latent,item_latent)
+        predict_vec = torch.mul(user_latent,item_latent)
         prediction = self.pred_layer(predict_vec)
 
-        output = F.softmax(prediction)
-        return output
-
+        output = F.softmax(prediction,dim=1)
+        return output.view(-1)
+    
 #TODO: Test instance
+if __name__ == "__main__":
+    args = parse_args()
+
+    data_path = join(args.path, "ratings.dat")
+
+    dataset = NCFDataset(
+        path=data_path,
+        num_neg=args.num_neg,
+        threshold=4
+    )
+
+    train_loader = DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=True
+    )
+
+    num_users = dataset.num_users
+    num_items = dataset.num_items
+
+    #Load Model
+    num_factors = args.num_factors
+    regs = eval(args.regs)
+
+    model = GMF(num_users,num_items,num_factors)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    loss_function = nn.BCELoss()
+
+    for epoch in range(args.epochs):
+
+        dataset.resample_negatives() # Don't forget to resample negatives every epoch for more variety!
+
+        train_loader = DataLoader(
+            dataset,
+            batch_size=args.batch_size,
+            shuffle=True
+        )
+
+        model.train()
+        total_loss = 0
+        start = time()
+
+        for user, item, label in train_loader:
+
+            prediction = model(user, item)
+            loss = loss_function(prediction, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        print(
+            "Epoch %d [%.1fs]: loss = %.4f"
+            % (epoch, time() - start, total_loss)
+        )
---
+++ NeuMF_torch.py
@@ -0,0 +1,11 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model():
+    pass
+
+if __name__ == "__main__":
+    pass
